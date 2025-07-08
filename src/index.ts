4e990925bb7c6ac794523412eb66ef84a8ba2f6d
rehtropsmb <139207294+rehtropsmb@users.noreply.github.com>
2025-07-08 00:03:10 -0500

    fix build failure

--- src/index.ts
+++ src/index.ts
@@ -201,27 +201,27 @@ client.on(Events.ClientReady, async () => {
                         const fuckKpen = data.filter(s => (s.profile.id == 35 && s.medal == 'silver'));
                         const toothy = data.filter(s => (s.profile.id == 85 && s.medal == 'silver'));
                         const midOff = client.channels.cache.get(
-                            '853532303779430460'
+                            '1207968143733948458'
                         ) as TextChannel;
 
                         if (fuckDread.length > 0) {
                             const diff = upload.score
-                                ? upload.record - fuckDread.record
-                                : Math.abs(upload.record).toFixed(2) - Math.abs(fuckDread.record).toFixed(2);
+                                ? upload.record - fuckDread[0].record
+                                : Math.abs(upload.record).toFixed(2) - Math.abs(fuckDread[0].record).toFixed(2);
                             await midOff.send(`<@269973086051368970> fuck you and your mid ass **${stageName}** [${diff}]`);
                         }
 
                         if (fuckKpen.length > 0) {
                             const diff = upload.score
-                                ? upload.record - fuckKpen.record
-                                : Math.abs(upload.record).toFixed(2) - Math.abs(fuckKpen.record).toFixed(2);
+                                ? upload.record - fuckKpen[0].record
+                                : Math.abs(upload.record).toFixed(2) - Math.abs(fuckKpen[0].record).toFixed(2);
                             await midOff.send(`<@204105879132438528> fuck you and your mid ass **${stageName}** [${diff}]`);
                         }
 
                         if (toothy.length > 0) {
                             const diff = upload.score
-                                ? upload.record - toothy.record
-                                : Math.abs(upload.record).toFixed(2) - Math.abs(toothy.record).toFixed(2);
+                                ? upload.record - toothy[0].record
+                                : Math.abs(upload.record).toFixed(2) - Math.abs(toothy[0].record).toFixed(2);
                             await midOff.send(`toothy record gone on **${stageName}** [${diff}]`);
                         }
                     }
